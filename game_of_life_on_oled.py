c8a34aa2691a52cb04b104b555e5a485d7f1822a
Daniel Raab <daniel.raab@mailbox.org>
2022-02-12 17:18:37 +0100

    run game of life on OLED

---
+++ game_of_life_on_oled.py
@@ -0,0 +1,19 @@
+"""Conway's Game of Life on a OLED display."""
+from PIL import Image
+import numpy as np
+
+import display
+from game_of_life import GameOfLife
+
+
+if __name__ == '__main__':
+    print('Initialising display...')
+    display.init()
+    print('Create Random Game of Life...')
+    gol = GameOfLife(display.WIDTH, display.HEIGHT, random=True)
+    print('Starting Game of Life.')
+    while True:
+        # convert numpy array to binary image
+        im = Image.fromarray(np.rot90(gol.grid))
+        display.show_image(im)
+        gol.next()
